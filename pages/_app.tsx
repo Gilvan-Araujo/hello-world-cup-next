1feb1a1ad279841641ca5602d21d7dba7e1e21c8
Gilvan Araujo <gilvanjunior@protonmail.com>
2022-11-29 07:30:24 -0300

    feat: early iteration of concept

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
